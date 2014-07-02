8a266814a320950b09f2a79381cf431c23d11f8c
v-wang <victorwang920@gmail.com>
2014-07-02 16:49:44 -0400

    styling to user signin index

--- config/routes.rb
+++ config/routes.rb
@@ -27,6 +27,8 @@ Rails.application.routes.draw do
 
   get 'user/destroy'
 
+  resources :users
+  resources :posts
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
